dd9eacb7d27486cf69082395fdef6b90eb493171
Valerii <cowr.valera@gmail.com>
2014-06-26 02:23:59 +0400

    add send comment

--- comment/urls.py
+++ comment/urls.py
@@ -3,5 +3,6 @@ from comment import views
 
 urlpatterns = patterns ('',
 		#url(r'^$', views.index, name='index'),
-		url(r'^(?P<comment_id>\d+)/$', views.comment, name='comment'),
+		url(r'^(?P<comment_id>\d+)/$', views.comment, name='comment'), 
+		url(r'^(?P<comment_id>\d+)/add/$', views.add_comment, name='add_comment'), 
 )
